111637504fdc9c1932d2ca9118573ccb7c5a8c35
Eman Gurung <egurung1117@gmail.com>
2024-07-13 16:50:01 -0400

    entering raw mode

--- kilo.c
+++ kilo.c
@@ -1,18 +1,44 @@
 #include <termios.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
+struct termios orig_termios;
 
-void enablerawMode() {
-    struct termois raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
     enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r", c, c);
+        }
+        if (c == 'q') break;
+    }
+  
+
 
     return 0;
 }
